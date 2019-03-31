3df22b693eb0f496a05c33c615646ff90685d399
Le Tan Thang <letanthang@gmail.com>
2019-03-31 18:35:50 +0700

    k8s deployment

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+export APP_NAME=hello-world
+export DATE=`date +%s`
+export RELEASE=staging
+  
+  function build() {
+    # registry_login
+        echo "Building Dockerfile-based application..."
+        #build image
+        docker build \
+          --build-arg HTTP_PROXY="$HTTP_PROXY" \
+          --build-arg http_proxy="$http_proxy" \
+          --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+          --build-arg https_proxy="$https_proxy" \
+          --build-arg FTP_PROXY="$FTP_PROXY" \
+          --build-arg ftp_proxy="$ftp_proxy" \
+          --build-arg NO_PROXY="$NO_PROXY" \
+          --build-arg no_proxy="$no_proxy" \
+          -t "letanthang/hello-world" .
+        
+        docker push letanthang/hello-world
+      
+  }
+  function deploy() {
+    #   sed 's/_VERSION_/'"latest"'/g; s/APP_NAME/'"$CI_PROJECT_NAME"'/g; s/_DATE_/'"$DATE"'/g; s/env-/'"$RELEASE-"'/g' ./provision/k8s/* > deployment.yaml
+      sed 's/APP_NAME/'"$APP_NAME"'/g; s/_DATE_/'"$DATE"'/g; s/env-/'"$RELEASE-"'/g' ./provision/k8s/deployment.yaml > deployment.yaml
+    #   sed 's/APP_NAME/'"$APP_NAME"'/g' ./provision/k8s/deployment.yaml > deployment.yaml
+      kubectl apply -f deployment.yaml
+  }
+  
+  build
+  deploy
+
+
+  # function install_dependencies() {
+  #   #install curl
+  #   apk add --no-cache curl
+    
+  #   # install kubectl
+  #   curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+  #   chmod +x ./kubectl
+  #   mv ./kubectl /usr/local/bin/kubectl
+    
+  #   #pre-config kubectl
+  #   mkdir ~/.kube
+  #   echo "$KUBE_STAGING" >> /tmp/kube_config
+  #   base64 -d /tmp/kube_config >> ~/.kube/config
+    
+  #   #install helm
+  #   curl "https://kubernetes-helm.storage.googleapis.com/helm-v${HELM_VERSION}-linux-amd64.tar.gz" | tar zx
+  #   mv linux-amd64/helm /usr/bin/
+  #   mv linux-amd64/tiller /usr/bin/
+  #   helm version --client
+  #   tiller -version
+  # }
