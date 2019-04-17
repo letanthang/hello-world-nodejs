e59b9fec60fc18a791157a9b27d245ea2080f6e0
Le Tan Thang <letanthang@gmail.com>
2019-04-17 11:47:36 +0700

    /form serve html upload form

--- index.js
+++ index.js
@@ -9,10 +9,23 @@
 // www.listen(8080);
 
 var http = require('http')
+var fs = require("fs");
+
+
 var server = http.createServer((request, response) => {
   console.log('We have a request for url: ' + request.url)
-  response.writeHead(200)
-  response.end('Hello from Thang123 with love!!!')
+  if (request.url === '/form') {
+    fs.readFile('form.html', function (err, data) {
+      response.writeHead(200, { 'Content-Type': 'text/html' })
+      response.write(data)
+      response.end()
+    })
+  } else {
+    response.writeHead(200)
+    response.end('Hello from Thang123 with love!!!')
+  }
+
+
 })
-server.listen(9090)
-console.log('http server at 9090')
+server.listen(3000)
+console.log('http server at 3000')
